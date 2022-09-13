b5c709bc4aa3b75f127ef0b8c5f7437763420495
Anton1802 <anton.sup1802@gmail.com>
2022-09-13 12:53:05 +0300

    function show_task, add_task

--- main.py
+++ main.py
@@ -2,10 +2,67 @@
 
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
+from kivy.uix.popup import Popup
+from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 
 
 class Reminder(FloatLayout):
-    pass
+
+    task = {}
+
+    def save_task(self, instance, title, description, time):
+        id = len(self.task) + 1
+        self.task[id] = {
+                        'title': title.text, 'description': description.text,
+                        'time': time.text
+                        }
+        button = Button(text=f"Task: {self.task[id]['title']}",
+                        size_hint_y=None,
+                        on_press=lambda x: self.show_task(id=id,
+                                                          instance=button),
+                        )
+        self.task_list.add_widget(button)
+
+    def add_task(self):
+        pop = Popup(size_hint=(.8, .7), title="New Task")
+        pop.open()
+        layout_main = GridLayout(cols=2)
+        layout_box_1 = BoxLayout(orientation="vertical", spacing=5)
+        layout_box_2 = BoxLayout(orientation="vertical", spacing=5)
+        layout_main.add_widget(layout_box_1)
+        layout_main.add_widget(layout_box_2)
+        label_task_title = Label(text="Title Task: ", size_hint_y=.1)
+        label_task_description = Label(text="Description Task: ",
+                                       size_hint_y=.1)
+        task_title = TextInput(size_hint_y=.1)
+        task_description = TextInput(size_hint_y=.1)
+        button_cancel = Button(text="Cancel", size=(100, 50),
+                               size_hint_y=None, on_press=pop.dismiss)
+        button_add = Button(text="Add", size=(100, 50),
+                            size_hint_y=None)
+        task_time = TextInput(size_hint_y=.1)
+        label_task_time = Label(text="Time Task: ",
+                                size_hint_y=.1)
+        layout_box_1.add_widget(label_task_title)
+        layout_box_1.add_widget(label_task_description)
+        layout_box_1.add_widget(label_task_time)
+        layout_box_1.add_widget(button_cancel)
+        layout_box_2.add_widget(task_title)
+        layout_box_2.add_widget(task_description)
+        layout_box_2.add_widget(task_time)
+        layout_box_2.add_widget(button_add)
+        pop.add_widget(layout_main)
+        button_add.bind(on_press=lambda x: self.save_task(title=task_title,
+                        description=task_description,
+                        time=task_time, instance=button_add))
+        button_add.bind(on_press=pop.dismiss)
+
+    def show_task(self, instance, id):
+        print(self.task[id])
 
 
 class ReminderApp(App):
