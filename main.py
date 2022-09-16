9e869e8a0defba9bb796a6a3bd3d9fd89f8039c4
Anton1802 <anton.sup1802@gmail.com>
2022-09-16 22:31:22 +0300

    add show_tasks create git ignore

--- main.py
+++ main.py
@@ -9,28 +9,26 @@ from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 import re
+import pickle
 
 
 class Reminder(FloatLayout):
 
-    task = {}
+    tasks = {}
 
     def save_task(self, instance, title, description, time):
         if self.time_valid(time.text):
-            id = len(self.task) + 1
-            self.task[id] = {
+            id = len(self.tasks) + 1
+            self.tasks[id] = {
                         'title': title.text, 'description': description.text,
                         'time': time.text
                         }
             button = Button(
-                        text=f"Task: {self.task[id]['title']}",
-                        size_hint_y=None,
-                        on_press=lambda x: self.show_task(
-                            id=id,
-                            instance=button
-                            ),
+                        text=f"Task: {self.tasks[id]['title']}",
+                        size_hint_y=None, on_press=self.show_task
                         )
             self.task_list.add_widget(button)
+            self.write_t()
         else:
             pop_error = Popup(title="Ошибка!", size_hint=(.8, .7))
             pop_error.add_widget(Label(
@@ -56,7 +54,6 @@ class Reminder(FloatLayout):
         button_add = Button(text="Add", size=(100, 50),
                             size_hint_y=None)
         task_time = TextInput(size_hint_y=.1)
-        self.task_time = task_time
         label_task_time = Label(text="Time Task: ",
                                 size_hint_y=.1)
         layout_box_1.add_widget(label_task_title)
@@ -73,16 +70,55 @@ class Reminder(FloatLayout):
                         time=task_time, instance=button_add))
         button_add.bind(on_press=pop.dismiss)
 
-    def show_task(self, instance, id):
-        print(self.task[id])
+    def show_task(self, instance):
+        str = instance.text
+        task_title = str.replace('Task: ', '')
+        for task in self.tasks:
+            if self.tasks[task]['title'] == task_title:
+                popup_task = Popup(title=self.tasks[task]['title'],
+                                   size_hint=(.7, .8))
+                popup_task.add_widget(Label(text="\n Описание: " +
+                                            self.tasks[task]['description']
+                                            + "\n Время: "
+                                            + self.tasks[task]['time']))
+                popup_task.open()
 
     def time_valid(self, text):
         return re.search('^[0-9]{2}:[0-9]{2}$', text)
 
+    def write_t(self):
+        with open("tasks.pickle", 'wb') as file:
+            pickle.dump(self.tasks, file)
+
+    def read_t(self):
+        try:
+            with open("tasks.pickle", 'rb') as file:
+                while True:
+                    self.tasks = pickle.load(file)
+        except EOFError:
+            pass
+        except FileNotFoundError:
+            pass
+        if len(self.tasks):
+            buttons = []
+            for task in self.tasks:
+                buttons.append(Button(
+                             text=f"Task: {self.tasks[task]['title']}",
+                             size_hint_y=None))
+
+            for bu in range(len(buttons)):
+                buttons[bu].bind(on_press=self.show_task)
+                self.task_list.add_widget(buttons[bu])
+
+    def start(self):
+        self.read_t()
+
 
 class ReminderApp(App):
     def build(self):
-        return Reminder()
+        reminder = Reminder()
+        reminder.start()
+        return reminder
 
 
 if __name__ == '__main__':
